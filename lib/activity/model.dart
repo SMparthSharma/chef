a262ceabb3b29d509d98ff5ce9fd77b410c144b8
SMparth sharma <darkcodeing0@gmail.com>
2024-10-14 17:07:54 +0530

    "move list to model file"

--- lib/activity/model.dart
+++ lib/activity/model.dart
@@ -3,22 +3,75 @@ class RecipeModel{
   late String image;
   late String recipe;
   late double calories;
+  late List<String> ing;
 
   RecipeModel(
       {this.image="pizza",
       this.foodLabel="pizza",
       this.calories=500.0,
-      this.recipe="milk",
+      this.recipe="milk"
       });
   factory RecipeModel.fromMap(Map recipe){
     return RecipeModel(
       foodLabel: recipe["label"],
       image: recipe["image"],
       calories: recipe["calories"],
-      recipe: recipe["url"]
+      recipe: recipe["url"],
 
     );
   }
 
 }
+List picPicker = [
+  {
+    "impUrl":
+    "https://www.tastingtable.com/img/gallery/20-delicious-indian-dishes-you-have-to-try-at-least-once/intro-1645057933.webp",
+    "headline": "Indian "
+  },
 
+  {
+    "impUrl":
+    "https://assets.clevelandclinic.org/transform/LargeFeatureImage/9cbcc77b-93b7-472e-838d-ec0d5f2f9fbd/heart-healthy-foods-1279763992",
+    "headline": "Healthy",
+  },
+  {
+    "impUrl":
+    "https://www.dumplingconnection.com/wp-content/uploads/2019/07/Chinese-cusine-e1586208090905.jpg",
+    "headline": "Chinese ",
+  },
+  {
+    "impUrl":
+    "https://hips.hearstapps.com/del.h-cdn.co/assets/15/24/1600x800/landscape-1433889344-del-milkshakes-index.jpg?resize=1200:*",
+    "headline": "Shakes",
+  },
+  {
+    "impUrl":
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU4RVhklejw3LtywA2YnTflm3fuEkbUsEyyw&s",
+    "headline": "Street Food",
+  },
+  {
+    "impUrl":
+    "https://www.linsfood.com/wp-content/uploads/2021/04/Red-Chilli-Paste-LinsFoodjpg.jpg",
+    "headline": "Chilli Food",
+  },
+  {
+    "impUrl":
+    "https://c4.wallpaperflare.com/wallpaper/626/14/515/dessert-sweet-food-cake-wallpaper-preview.jpg",
+    "headline": "Sweet",
+  },
+  {
+    "impUrl":
+    "https://nutritionrefined.com/wp-content/uploads/2023/08/garden-salad.jpg",
+    "headline": "Salad",
+  },
+  {
+    "impUrl":
+    "https://assets.rbl.ms/21919567/origin.jpg",
+    "headline": "Ice Cream",
+  },
+  {
+    "impUrl":
+    "https://img.freepik.com/premium-photo/food-photos_662583-2610.jpg?w=740",
+    "headline": "Fast Food",
+  },
+];
